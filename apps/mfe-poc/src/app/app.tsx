a33d64410d9bb12da5b7c94ec38e1d6afa1a8fbe
ethantaylan <ethtaylan@gmail.com>
2025-04-28 17:13:40 +0200

    Initial commit

---
+++ apps/mfe-poc/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="mfe-poc" />
+    </div>
+  );
+}
+
+export default App;
